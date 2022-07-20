3bc963057345a763e23643967085b332060aa9d0
Biman <biman20006@iiitd.ac.in>
2022-07-20 17:18:33 +0530

    [Zenith] Refactor. Build step seperate into build and deployToQA steps

--- src/main/resources/db/migration/V3_Update_book.sql
+++ src/main/resources/db/migration/V3_Update_book.sql
@@ -1,8 +1,8 @@
-ALTER TABLE IF EXISTS public.books
+ALTER TABLE books
     ADD COLUMN availability boolean DEFAULT TRUE;
-ALTER TABLE IF EXISTS public.books
+ALTER TABLE books
     ADD COLUMN description text DEFAULT NULL;
-ALTER TABLE IF EXISTS public.books
+ALTER TABLE books
     ADD COLUMN ratings integer DEFAULT 0;
-ALTER TABLE IF EXISTS public.books
+ALTER TABLE books
     ADD COLUMN reviews text[] DEFAULT NULL;
